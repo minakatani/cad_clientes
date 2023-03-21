722d1b49b355e946d6ea10abffa1349d62a7e108
minakatani <infonakatani@gmail.com>
2023-03-21 05:38:03 -0300

    Versao 1.0.0

---
+++ lib/homepage.dart
@@ -0,0 +1,96 @@
+import 'dart:html';
+import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class Pessoa {
+  final String nome;
+  final String telefone;
+  final String email;
+
+  Pessoa({required this.nome, required this.telefone, required this.email});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'nome': nome,
+      'telefone': telefone,
+      'email': email,
+    };
+  }
+}
+
+class CadastroPessoas extends StatefulWidget {
+  const CadastroPessoas({super.key});
+
+  @override
+  _CadastroPessoasState createState() => _CadastroPessoasState();
+}
+
+class _CadastroPessoasState extends State<CadastroPessoas> {
+  final _nomeController = TextEditingController();
+  final _telefoneController = TextEditingController();
+  final _emailController = TextEditingController();
+
+  late final DatabaseReference _pessoaRef;
+
+  @override
+  void initState() {
+    super.initState();
+    _pessoaRef = FirebaseDatabase.instance.ref('pessoa.db');
+  }
+
+  @override
+  void dispose() {
+    _nomeController.dispose();
+    _telefoneController.dispose();
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  void _cadastrar() {
+    final nome = _nomeController.text;
+    final telefone = _telefoneController.text;
+    final email = _emailController.text;
+    final pessoa = Pessoa(nome: nome, telefone: telefone, email: email);
+    _pessoaRef.push().set(pessoa.toMap());
+    _nomeController.clear();
+    _telefoneController.clear();
+    _emailController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastro de pessoas'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: _nomeController,
+                decoration: InputDecoration(labelText: 'Nome'),
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                controller: _telefoneController,
+                decoration: InputDecoration(labelText: 'Telefone'),
+              ),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _cadastrar,
+                child: Text('Cadastrar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:cad_clientes/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cadastro de Clientes',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const CadastroPessoas(),
+    );
+  }
+}
