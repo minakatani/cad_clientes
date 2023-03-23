e6da526576debf84969c34fc05c4eea7b7b564cb
minorunakatani <infonakatani@gmail.com>
2023-03-23 20:57:46 -0300

    Adicinado pagina de cadastro

---
+++ lib/add_user.dart
@@ -0,0 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class AddCliente extends StatelessWidget {
+  final _nomecompleto = TextEditingController();
+  final _telefone = TextEditingController();
+  final _email = TextEditingController();
+
+//  AddCliente(this._nomecompleto, this._telefone, this._email,);
+
+  CollectionReference clientes =
+      FirebaseFirestore.instance.collection('clientes');
+
+  Future<void> addCliente() {
+    return clientes
+        .add({
+          'nomecompleto': _nomecompleto,
+          'telefone': _telefone,
+          'email': _email
+        })
+        .then((value) => print("Cliente cadastrado com sucesso"))
+        .catchError((error) => print("Falha ao cadastrar o cliente: $error"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastro de pessoas'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: _nomecompleto,
+                decoration: InputDecoration(labelText: 'Nome'),
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                controller: _telefone,
+                decoration: InputDecoration(labelText: 'Telefone'),
+              ),
+              TextFormField(
+                controller: _email,
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: addCliente,
+                child: Text('Cadastrar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:cad_clientes/homepage.dart';
+import 'package:cad_clientes/add_user.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: const CadastroPessoas(),
+      home: AddCliente(),
     );
   }
 }
