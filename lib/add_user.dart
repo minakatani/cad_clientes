d058c019bcada88040dd3516eb18a732d75d25d2
minorunakatani <infonakatani@gmail.com>
2023-03-25 18:37:31 -0300

    Adicionado testes com models e repository em arquivos separados

--- lib/add_user.dart
+++ lib/add_user.dart
@@ -1,35 +1,46 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:cad_clientes/models/cliente.dart';
 
 class AddCliente extends StatefulWidget {
-  const AddCliente({super.key});
+  AddCliente({
+    super.key,
+  });
 
   @override
   State<AddCliente> createState() => _AddClienteState();
+
+  CollectionReference clientes =
+      FirebaseFirestore.instance.collection('clientes');
 }
 
 class _AddClienteState extends State<AddCliente> {
-  TextEditingController? _nomecompleto;
-  TextEditingController? _telefone;
-  TextEditingController? _email;
-
-//  AddCliente(this._nomecompleto, this._telefone, this._email,);
-  CollectionReference clientes =
-      FirebaseFirestore.instance.collection('clientes');
-
-  Future<void> addCliente() {
-    return clientes
-        .add({
-          'nomecompleto': _nomecompleto,
-          'telefone': _telefone,
-          'email': _email
-        })
-        .then((value) => print("Cliente cadastrado com sucesso"))
-        .catchError((error) => print("Falha ao cadastrar o cliente: $error"));
-  }
-
   @override
   Widget build(BuildContext context) {
+    final nomecompleto = TextEditingController();
+    final telefone = TextEditingController();
+    final email = TextEditingController();
+
+    Map<String, dynamic> toMap() {
+      return {
+        'nomecompleto': nomecompleto,
+        'telefone': telefone,
+        'email': email,
+      };
+    }
+
+    addCliente() {
+      var cliente;
+      return cliente
+          .add({
+            'nomecompleto': nomecompleto,
+            'telefone': telefone,
+            'email': email
+          })
+          .then((value) => print("Cliente cadastrado com sucesso"))
+          .catchError((error) => print("Falha ao cadastrar o cliente: $error"));
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Cadastro de pessoas'),
@@ -41,16 +52,16 @@ class _AddClienteState extends State<AddCliente> {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               TextFormField(
-                controller: _nomecompleto,
+                controller: nomecompleto,
                 decoration: InputDecoration(labelText: 'Nome'),
               ),
               TextFormField(
                 keyboardType: TextInputType.number,
-                controller: _telefone,
+                controller: telefone,
                 decoration: InputDecoration(labelText: 'Telefone'),
               ),
               TextFormField(
-                controller: _email,
+                controller: email,
                 decoration: InputDecoration(labelText: 'Email'),
               ),
               SizedBox(height: 16),
---
+++ lib/models/cliente.dart
@@ -0,0 +1,11 @@
+class Cliente {
+  String nomecompleto;
+  String telefone;
+  String email;
+
+  Moeda({
+    required this.nomecompleto,
+    required this.telefone,
+    required this.email,
+  });
+}
